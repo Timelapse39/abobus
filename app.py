59a83efc09dc3630c37175ea1dbcb75e8d2a3d0c
adder013 <yagod99@gmail.com>
2023-01-15 22:00:28 +0500

    Added streamlit app

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+from model import Prediction
+
+file = st.file_uploader("Upload an image", key="image", type=["png", "jpg", "jpeg"])
+
+if file is not None:
+    image = Image.open(file)
+
+    st.image(
+        image,
+        caption=f"You amazing image has shape",
+        use_column_width=True,
+    )
+
+if st.session_state.image:
+    answer_dict = (Prediction.get_prediction(image)[0])
+
+    st.text_input('Ответ', value=answer_dict['generated_text'], disabled=True)
